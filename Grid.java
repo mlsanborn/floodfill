8d11b4e8016c7843e6f30e8e9d9c6970060d850d
mlsanborn <mlsanborn@stu.naperville203.org>
2017-12-07 22:16:08 -0600

    Revert "hkfhtsjyrj"
    This reverts commit 01e074b12751506cb939af93bae3e94c87b7fa48.

--- Grid.java
+++ Grid.java
@@ -3,6 +3,7 @@ public class Grid
    private static final int SIZE = 10;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
    int[][] pixels = new int[SIZE][SIZE]; // fills 10 x 10 with 0's
    Stack<Integer> fill = new Stack<>();
    boolean cont = true;
@@ -24,11 +25,17 @@ public class Grid
    . . .
 
 >>>>>>> parent of 01e074b... hkfhtsjyrj
+=======
+   int[][] pixels = new int[SIZE][SIZE];
+   . . .
+
+>>>>>>> parent of 01e074b... hkfhtsjyrj
    /**
       Flood fill, starting with the given row and column.
    */
    public void floodfill(int row, int column)
    {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
        int start = ((row) * 10) + (col) ; // where to start in stack
@@ -84,6 +91,9 @@ public class Grid
             //System.out.print("help"+num); // test
         }
        
+=======
+      . . .
+>>>>>>> parent of 01e074b... hkfhtsjyrj
 =======
       . . .
 >>>>>>> parent of 01e074b... hkfhtsjyrj
